8b606ac3f5866bcb199eb19c732bee8b5f32e53d
Ethan Nguyen <45704850+ethanNguyenUser@users.noreply.github.com>
2018-12-08 19:38:26 -0800

    Update ConvexHull.java

--- ConvexHull.java
+++ ConvexHull.java
@@ -4,8 +4,10 @@ import javax.swing.JFrame;
  
 public class ConvexHull {
 	
-	public static int DISPLAY_WIDTH = 1000;
-	public static int DISPLAY_HEIGHT = 1000;
+	public final static int DISPLAY_WIDTH = 900;
+	public final static int DISPLAY_HEIGHT = 900;
+	private final static boolean IS_RESIZABLE = false;
+	
 	
 	public static void main(String[] args) {
 		
@@ -16,6 +18,7 @@ public class ConvexHull {
 		f.setTitle("Ethan Nguyen's Convex Hull");
 		Display display = new Display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
 		f.add(display);
+		f.setResizable(IS_RESIZABLE);
 		f.setVisible(true);
 		display.runLoop();
 	}
